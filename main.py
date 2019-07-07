cdb0791db00f599acdaf31df6bac30d2cec4b856
Quentin Duchemin <quentinduchemin@tuta.io>
2019-07-07 14:09:14 +0200

    Remove absolute bullshit code
    Idk why but at the beginning I thought it was a great idea to separate containers managed by Docker Compose and containers runned manually.
    I remember that I wanted to parse docker-compose.yml file to get Traefik labels.
    
    Anyway :
    * As more and more services will be managed by CI, no global docker-compose.yml file will be maintained
    * We can get Traefik label from the Docker API, so... just remove all this part
    
    Also try to make a class even if I know nothing about python

--- main.py
+++ main.py
@@ -8,88 +8,60 @@ import socket
 import os
 from ruamel.yaml import YAML
 
-DIR_NAME = os.path.dirname(os.path.realpath(__file__))
-CONFIG_FILE = os.path.join(DIR_NAME, 'config.json')
-DOCKER_COMPOSE_FILE = os.path.join(DIR_NAME, 'docker-compose.yml')
-OUTPUT_DIR = os.path.join(DIR_NAME, 'output')
+class GraphBot:
+	'''
+	This class asks the Docker daemon informations about running Containers
+	and constructs a graph showing dependencies between containers.
 
-def get_containers(docker_client):
-	''' 
-	Get running docker containers on the host described by docker_client
-	
-	:param docker_client: docker client for target host
-	:type docker_client: DockerClient
-	:rtype (dict, set)
-	:returns The dict describes containers runned by Docker Compose {service_name: <config>}. The set contains container names not runned by Docker Compose<
+	We also use Traefik labels to show links between the reverse proxy and the containers.
 	'''
 
-	# Services described by Docker Compose file 
-	dc_services = {}
+	DIR_NAME = os.path.dirname(os.path.realpath(__file__))
 
-	# Names of all running containers
-	running_containers = []
+	def __init__(self, config_path = DIR_NAME, output_dir = DIR_NAME):
+		# Get configuration
+		with open(os.path.join(config_path, 'config.json'), 'r') as fd:
+			self.config = json.load(fd)
+		self.OUTPUT_DIR = os.path.join(config_path, 'output')
+		self.docker_client = docker.from_env()
 
-	# Get all services declared in Docker Compose
-	with open(DOCKER_COMPOSE_FILE, 'r') as f:
-		dc = YAML(typ='safe').load(f)
-		dc_services = dict(dc['services'])
+	def build_graph(self):
+		running = self.__get_containers()
+		g = graphviz.Digraph(comment = 'Machine physique : {0}'.format(self.config['machine_name']), format = 'png')
+		g.attr(label = 'Machine physique : {0}'.format(self.config['machine_name']))
+		with g.subgraph(name = 'cluster_0') as vm:
+			vm.attr(label = 'Machine virtuelle : {0}'.format(socket.gethostname()))
+			vm.attr(color = 'lightgrey')
+			vm.node_attr.update(style = 'filled', color = 'orange')
+			for c in running:
+				vm.node(c.name)
 
-	# Get all running containers
-	for container in docker_client.containers.list():
-		if container.status == 'running':
-			running_containers.append(container.name)
+		# Create PNG
+		g.render(os.path.join(self.OUTPUT_DIR, '{0}.gv'.format(socket.gethostname())))
 
-	# Just for testing in local
-	running_containers.append('traefik')
-	running_containers.append('mattermost')
-	running_containers.append('mattermost-db')
+	def __get_containers(self):
+		'''
+		Get running docker containers on the host described by docker_client, without those excluded in configuration
 
-	# Get all running containers declared in Docker Compose
-	dc_containers = dict(filter(lambda x: x[0] in running_containers, dc_services.items()))
+		:param docker_client: docker client for target host
+		:type docker_client: DockerClient
+		:rtype List
+		:returns List of Containers (running containers)
+		'''
 
-	# Get all running containers not declared in Docker Compose
-	stdr_containers = set(running_containers) - dc_containers.keys()
+		# Names of all running containers
+		running_containers = []
 
-	return (dc_containers, stdr_containers)
+		# Get all running containers
+		for container in self.docker_client.containers.list():
+			if container.status == 'running' and container.name not in self.config['exclude']:
+				running_containers.append(container)
 
-	# TODO exclude containers from config
+		return running_containers
 
 def main():
-	# Get configuration
-	with open(CONFIG_FILE, 'r') as fd:
-		config = json.load(fd)
-
-	# Get containers split by Docker Compose management criteria
-	client = docker.from_env()
-	dc_containers, stdr_containers = get_containers(client)
-
-	# TODO put in func
-	# Add running containers as node, with different colors depending of management by Docker Compose
-	g = graphviz.Digraph(comment = 'Machine physique : {0}'.format(config['machine_name']), format = 'png')
-	g.attr(label = 'Machine physique : {0}'.format(config['machine_name']))
-	with g.subgraph(name = 'cluster_0') as vm:
-		vm.attr(label = 'Machine virtuelle : {0}'.format(socket.gethostname()))
-		# Create subgraph containing all running container managed by Docker Compose
-		with vm.subgraph(name = 'cluster_0_0') as dc:
-			# TODO refactor styling
-			dc.attr(label = 'Lancés par Docker Compose')
-			dc.attr(style = 'filled')
-			dc.attr(color = 'lightgrey')
-			dc.node_attr.update(style = 'filled', color = 'orange')
-			for c in dc_containers.keys():
-				dc.node(c)
-
-		# Create subgraph containing all running container runned with Docker command
-		with vm.subgraph(name = 'cluster_0_1') as stdr:
-			stdr.attr(label = 'Lancés par une commande Docker')
-			stdr.attr(style = 'filled')
-			stdr.attr(color = 'lightgrey')
-			stdr.node_attr.update(style = 'filled', color = 'green')
-			for c in stdr_containers:
-				stdr.node(c)
-
-	# Create PNG 
-	g.render(os.path.join(OUTPUT_DIR, '{0}.gv'.format(socket.gethostname())))
+	graph = GraphBot()
+	graph.build_graph()
 
 if __name__ == '__main__':
     main()
